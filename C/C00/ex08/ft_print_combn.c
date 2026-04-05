fab1d4919266528cffcc4d5812386ca3b3fd9e0a
Thomasrbm <frozyxyt.76@gmail.com>
2026-04-05 14:57:18 +0200

    Add: printcombn, find the array approach

--- C/C00/ex08/ft_print_combn.c
+++ C/C00/ex08/ft_print_combn.c
@@ -6,16 +6,28 @@
 
 void ft_print_combn(int n)
 {
+	int nb[n];
+	int len = 0;
 
 
+	while (len < n)
+	{
+		nb[len] = len;
+		printf("%d\n", nb[i]);
+		len++;
+	}
 }
 
 
 
+// regle 2 : n1 + 1   n2 = n1 + 1.     si last = 9
 
+
+//  3 = n2
+// 9  = n8 + 1
 
 int main()
 {
-	ft_print_combn(4);
+	ft_print_combn(9);
 	return 0;
 }
